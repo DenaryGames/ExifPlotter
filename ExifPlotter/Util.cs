d94a4a4ac2077d564b6d1d28bfb16d35217b2308
c3jasalm <jaakko.salminen@cs.tamk.fi>
2016-02-14 11:16:52 +0200

    Picture Files Collection & Util, WIP

---
+++ ExifPlotter/Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExifPlotter
+{
+    class Util
+    {
+        public PictureFiles FindFiles()
+        {
+            // Create PictureFiles object
+            // Find files
+            // Add individual pictures
+            // Populate PictureFiles statistics
+
+            return null;
+        }
+    }
+}
